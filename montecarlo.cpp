1e53747f3551723f69d7bf8d891343a13bed0d19
omynayak <manelomnayak099@gmail.com>
2025-12-04 05:31:07 +0530

    Minor changes

--- montecarlo.cpp
+++ montecarlo.cpp
@@ -1,21 +1,21 @@
 #include "montecarlo.h"
 
-Xorshift32 coin(123);
-int coinToss();
+int coinToss(Xorshift32 &coin);
 
 double montecarlo(){
+	Xorshift32 coin(123);
 	int value = 0;
 	uint32_t n = 1000000000;
 
 	uint32_t headCount = 0;
 	for(uint32_t i = 0; i < n; i++){
-		value = coinToss();
+		value = coinToss(coin);
 		if(value == 1) headCount++;
 	}
 
 	return ((double) headCount / n);
 }
 
-int coinToss(){
+int coinToss(Xorshift32 &coin){
 	return (2 * coin.nextDouble());
 }
